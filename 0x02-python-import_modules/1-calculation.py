61b32655e9ae27efaedbba412fb37d1ea7d63dca
Nosenti <inno100os@gmail.com>
2024-01-18 16:17:51 +0200

    formatting 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,8 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-    a, b = 10, 5
+    a = 10
+    b = 5
     print("{} + {} = {}".format(add(a, b)))
     print("{} - {} = {}".format(sub(a, b)))
     print("{} * {} = {}".format(mul(a, b)))
