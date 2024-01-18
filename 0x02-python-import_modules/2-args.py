26856a51c3b9831bf46c8ba332039e61100d9351
Nosenti <inno100os@gmail.com>
2024-01-19 01:36:27 +0200

    number 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@
 if __name__ == "__main__":
     import sys
 
-    num_args = len(sys.argv)
-    if num_args > 1:
-        if num_args == 2:
-            print("{} argument:".format(num_args - 1))
-        else:
-            print("{} arguments:".format(num_args - 1))
+    num_args = len(sys.argv) - 1
 
-        for i in range(1, num_args):
-            print("{}: {}".format(i, sys.argv[i]))
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("{} argument:".format(num_args))
     else:
-        print("0 arguments")
+        print("{} arguments:".format(num_args))
+
+    for i in range(1, num_args + 1):
+        print("{}: {}".format(i, sys.argv[i]))
