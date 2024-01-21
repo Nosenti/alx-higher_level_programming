dd6b53a3b26314b667230d68bbdae3c6c6de5260
Nosenti <inno100os@gmail.com>
2024-01-21 22:20:31 +0200

    add edge cases

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,10 @@
 
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
-
+    if my_list is None:
+        return None
+    if length == 0:
+        return my_list
     i = 0
     while (i < length):
         print("{:d}".format(my_list[length - 1 - i]))
