06f86cff2190f6f00525af909d5edfd9bcbed057
Nosenti <inno100os@gmail.com>
2024-01-22 14:28:42 +0200

    switch returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
     s_len = len(sentence)
     first_char = sentence[0]
-    if s_len == 0:
-        return (s_len, None)
-    else:
+    if s_len > 0:
         return (s_len, first_char)
+    else:
+        return (s_len, None)
