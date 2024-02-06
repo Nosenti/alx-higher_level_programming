bd5eda18e41aca449b7e31cdd2a0bab59d7f03d3
Nosenti <inno100os@gmail.com>
2024-02-06 22:07:29 +0200

    first 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class the defines a square
+"""
+
+class Square:
+    """class with private attribute - size
+    """
+    def __init__(self, size):
+        """size variable initialized as a private instance attribute
+        """
+        self.__size = size
