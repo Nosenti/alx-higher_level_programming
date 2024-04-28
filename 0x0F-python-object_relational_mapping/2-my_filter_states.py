2306f85a65ef285338bdf035d8c517546f56bb4b
Nosenti <inno100os@gmail.com>
2024-04-28 23:26:43 +0200

    using format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,9 @@ def list_states(username, password, db_name, state_name):
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=db_name)
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
+    query = """SELECT * FROM states
+    WHERE name = '{}' ORDER BY id ASC""".format(state_name)
+    cur.execute(query)
     states = cur.fetchall()
     for state in states:
         print(state)
