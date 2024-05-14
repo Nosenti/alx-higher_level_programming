f1b2ad4ba94fa3cb21b0160b4b85c94c55b81507
Nosenti <inno100os@gmail.com>
2024-05-14 12:23:56 +0200

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a Post request to URL with an email as a parameter
+and displays the body
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
