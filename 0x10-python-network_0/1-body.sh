ffafcb461403620d673fffcf5fef2d853873e7f5
Nosenti <inno100os@gmail.com>
2024-05-14 09:57:45 +0200

    format the curl command

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a GET request to URL and display the body of a 200 status code response
-curl -sL -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -sLw "%{http_code}" "$1" -o /dev/null | grep -q 200 && curl -s "$1"
